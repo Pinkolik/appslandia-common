193eb107ae99d2a2a50ead59ac9b127c9bf9a42e
Loc Ha <haducloc13@gmail.com>
2021-10-13 13:36:04 -0400

    Remove fixIdSeq

--- src/main/java/com/appslandia/common/jdbc/JdbcUtils.java
+++ src/main/java/com/appslandia/common/jdbc/JdbcUtils.java
@@ -21,18 +21,14 @@
 package com.appslandia.common.jdbc;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import com.appslandia.common.utils.AssertUtils;
 import com.appslandia.common.utils.ObjectUtils;
-import com.appslandia.common.utils.StringFormat;
 
 /**
  *
@@ -115,67 +111,5 @@ public class JdbcUtils {
 			labels.add(md.getColumnLabel(idx));
 		}
 		return labels.toArray(new String[labels.size()]);
-	}
-
-	public static long fixIdSeq(String tableName, String idColumn, boolean idInt, String idPkSeq, String idNotNull, String idPk, String idSeq, Connection conn)
-			throws SQLException {
-		AssertUtils.assertNotNull(tableName);
-		AssertUtils.assertNotNull(idColumn);
-
-		if (idPkSeq == null) {
-			AssertUtils.assertNotNull(idNotNull);
-			AssertUtils.assertNotNull(idPk);
-			AssertUtils.assertNotNull(idSeq);
-		}
-
-		long seq = 0;
-		try (Statement stat = conn.createStatement()) {
-
-			// Add tempIdCol
-			String tempIdCol = idColumn + "_TEMP";
-			stat.executeUpdate(StringFormat.fmt("ALTER TABLE {} ADD {} {}", tableName, tempIdCol, (idInt ? "INT" : "BIGINT")));
-
-			// Update tempIdCol
-			try (PreparedStatement updStat = conn.prepareStatement(StringFormat.fmt("UPDATE {} SET {}=? WHERE {}=?", tableName, tempIdCol, idColumn))) {
-				try (ResultSet rs = stat.executeQuery(StringFormat.fmt("SELECT {} FROM {} ORDER BY {}", idColumn, tableName, idColumn))) {
-
-					while (rs.next()) {
-						seq++;
-
-						if (idInt) {
-							updStat.setInt(1, (int) seq);
-							updStat.setInt(2, rs.getInt(1));
-						} else {
-							updStat.setLong(1, seq);
-							updStat.setLong(2, rs.getLong(1));
-						}
-						updStat.addBatch();
-
-						if (seq % 100 == 0)
-							updStat.executeBatch();
-					}
-				}
-
-				updStat.executeBatch();
-			}
-
-			// Re-create ID
-			stat.executeUpdate(StringFormat.fmt("ALTER TABLE {} DROP COLUMN {}", tableName, idColumn));
-			stat.executeUpdate(StringFormat.fmt("ALTER TABLE {} ADD {} {}", tableName, idColumn, (idInt ? "INT" : "BIGINT")));
-			stat.executeUpdate(StringFormat.fmt("UPDATE {} SET {}={}", tableName, idColumn, tempIdCol));
-
-			if (idPkSeq != null) {
-				stat.executeUpdate(StringFormat.fmt("ALTER TABLE {} {}", tableName, idPkSeq));
-			} else {
-				stat.executeUpdate(StringFormat.fmt("ALTER TABLE {} {}", tableName, idNotNull));
-				stat.executeUpdate(StringFormat.fmt("ALTER TABLE {} {}", tableName, idPk));
-				stat.executeUpdate(StringFormat.fmt("ALTER TABLE {} {}", tableName, idSeq));
-			}
-
-			// Drop tempIdCol
-			stat.executeUpdate(StringFormat.fmt("ALTER TABLE {} DROP COLUMN {}", tableName, tempIdCol));
-
-			return seq;
-		}
 	}
 }
